dcc230f3c9f4779b0650203c3330765ceae71f00
Timothy Lee <timmy.ccl@gmail.com>
2014-05-11 03:51:45 +0800

    Link 加上功能

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
-  var a = 1;
-  //alert($('.content').css('flex'));
+  var render_index = function(path) {
+    $.get('/api/getindex/' + path, function(res) {
+      $('.index').children().remove();
+      $.each(res, function(index, r) {
+        var li = $('<li></li>').html($('<a></a>').attr('href', r.url).text(r.name));
+        $('.index').append(li);
+      });
+    });
+  }
+
+  var match = window.location.hash.match(/#(.+)/);
+  var path = "";
+  if (match != null) {
+    path = match[1];
+  }
+  render_index(path);
+
+  $('.menu a:not(.dropdown-toggle)').click(function(e) {
+    path = $(this).attr('href').match(/#(.+)/)[1];
+    render_index(path);
+  });
 });
