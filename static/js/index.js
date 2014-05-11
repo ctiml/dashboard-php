ab6974a2713a75b673fae8fde6937da209a6325f
Timothy Lee <timmy.ccl@gmail.com>
2014-05-11 16:11:27 +0800

    左邊 index 加上功能

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,90 @@
-$(document).ready(function() {
-  var render_index = function(path) {
-    $.get('/api/getindex/' + path, function(res) {
-      $('.index').children().remove();
-      $.each(res, function(index, r) {
-        var li = $('<li></li>').html($('<a></a>').attr('href', r.url).text(r.name));
-        $('.index').append(li);
+var Index = Index || (function() {
+  var _container = null;
+  var _target = null;
+  var _create = function(index) {
+    if (_container === null || _container === undefined) {
+      return;
+    } else {
+      _container.children().remove();
+    }
+    var ul = $('<ul></ul>');
+    $.each(index, function(i, ei) {
+      var li = _createItem();
+      li.find('a').attr('href', ei.url);
+      li.find('span').text(ei.name);
+      if (ei.folder) {
+        li.find('i').addClass('fa fa-caret-down pull-right');
+        $.each(ei.folder, function(fi, fei) {
+          var subItem = $('<ul></ul>').append(_createItem());
+          subItem.find('a').attr('href', fei.url);
+          subItem.find('span').text(fei.name);
+          li.append(subItem);
+        });
+      }
+      ul.append(li);
+    })
+    _container.append(ul);
+  }
+  var _createItem = function() {
+    return $('<li><div><a class="link"><span></span><i></i></a></div></li>');
+  }
+  var _registerEvents = function() {
+    _container.find('a.link').click(function(e) {
+      e.preventDefault();
+      var url = $(this).attr('href');
+      if (_isSupported(url) && _target !== null && _target !== undefined) {
+        _target.find('.iframe-container').hide();
+        var iframe = _target.find('.iframe-container[src="' + url + '"]');
+        if (iframe.length > 0) {
+          iframe.show();
+        } else {
+          var iframeNew = $('<iframe></iframe>').addClass('iframe-container').attr('src', url).show();
+          _target.append(iframeNew);
+        }
+        _container.find('li').removeClass('active');
+        $(this).parents('li').addClass('active');
+      } else {
+        window.open(url);
+      }
+    });
+    _container.find('a.link i').click(function(e) {
+      e.preventDefault();
+      var self = $(this);
+      var ul = self.parents('li').find('ul');
+      ul.slideToggle(function() {
+        self.removeClass('fa-caret-down fa-caret-right').addClass(ul.is(':visible') ? 'fa-caret-down' : 'fa-caret-right');
       });
+      return false;
+    });
+  }
+  var _isSupported = function(url) {
+    if (!url) return false;
+    if (url.match(/^http(s)?:\/\/docs\.google\.com/)) { return true; }
+    if (url.match(/^http(s)?:\/\/\w+\.hackpad\.com/)) { return true; }
+    if (url.match(/^http(s)?:\/\/ethercalc\.org\//)) { return true; }
+    return false;
+  }
+
+  return {
+    init: function(container, target) {
+      _container = container;
+      _target = target;
+    },
+    create: function(index) {
+      _create(index);
+      _registerEvents();
+    }
+  }
+})();
+
+$(document).ready(function() {
+
+  Index.init($('.index'), $('.content'));
+
+  var getIndex = function(path) {
+    $.get('/api/getindex/' + path, function(res) {
+      Index.create(res);
+      $(window).trigger('resize');
     });
   }
 
@@ -14,10 +93,16 @@ $(document).ready(function() {
   if (match != null) {
     path = match[1];
   }
-  render_index(path);
+  getIndex(path);
 
   $('.menu a:not(.dropdown-toggle)').click(function(e) {
     path = $(this).attr('href').match(/#(.+)/)[1];
-    render_index(path);
+    getIndex(path);
+  });
+
+  $(window).resize(function(ev){
+    var height = $(window).height() - $('.navbar').height();
+    $('.index').height(height);
+    $('.content').height(height);
   });
 });
